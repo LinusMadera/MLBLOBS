7272eaa03f819bce44c9c4b1b4b484ddf1565a5f
LinusMadera <linus.madera@gmail.com>
2024-05-17 19:13:32 -0300

    name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
 const App = () => {
-  const redirectUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vTw6PpsIx4qo4OEaENcK6__SEgpoydXGkW44ZGuYbSbhM1E4wOT5C1EfvP3IN8mB7KcIx1Sze7p0Jat/pub?start=false&loop=false&delayms=60000'; // Replace with your desired URL
+  const redirectUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vTw6PpsIx4qo4OEaENcK6__SEgpoydXGkW44ZGuYbSbhM1E4wOT5C1EfvP3IN8mB7KcIx1Sze7p0Jat/pub?start=false&loop=false&delayms=60000';
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to={redirectUrl} replace />} />
-      </Routes>
-    </Router>
-  );
+  useEffect(() => {
+    window.location.href = redirectUrl;
+  }, []);
+
+  return null;
 };
 
 export default App;
