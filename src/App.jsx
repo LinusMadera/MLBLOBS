24f3fbd0b035e7dfd68695895c14104cb73cc8a4
LinusMadera <linus.madera@gmail.com>
2024-05-17 19:01:53 -0300

    eba 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 const App = () => {
   const redirectUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vTw6PpsIx4qo4OEaENcK6__SEgpoydXGkW44ZGuYbSbhM1E4wOT5C1EfvP3IN8mB7KcIx1Sze7p0Jat/pub?start=false&loop=false&delayms=60000'; // Replace with your desired URL
 
   return (
     <Router>
-      <Route path="/" render={() => <Redirect to={redirectUrl} />} />
+      <Routes>
+        <Route path="/" element={<Navigate to={redirectUrl} replace />} />
+      </Routes>
     </Router>
   );
 };
