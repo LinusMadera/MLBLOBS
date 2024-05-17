dfcc2c6d96c1cdf7645742017a32679826b55c31
LinusMadera <linus.madera@gmail.com>
2024-05-17 19:16:17 -0300

    more time

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 const App = () => {
-  const redirectUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vTw6PpsIx4qo4OEaENcK6__SEgpoydXGkW44ZGuYbSbhM1E4wOT5C1EfvP3IN8mB7KcIx1Sze7p0Jat/pub?start=false&loop=false&delayms=60000';
+  const redirectUrl = 'https://docs.google.com/presentation/d/e/2PACX-1vTw6PpsIx4qo4OEaENcK6__SEgpoydXGkW44ZGuYbSbhM1E4wOT5C1EfvP3IN8mB7KcIx1Sze7p0Jat/pub?start=false&loop=false&delayms=6000000';
 
   useEffect(() => {
     window.location.href = redirectUrl;
